c260ec6d6c44b473c813673b823b6d16ed8a295b
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-19 10:11:19 +0200

    add acceptance criteria for tests to pass

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Acceptance Criteria:
+// Passed: You should provide your own project, not the example URL.
+// Passed: A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
+// Passed: A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+// Failed: A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+// Failed: Your project can handle dates that can be successfully parsed by new Date(date_string)
+// Passed: If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
+// Failed: An empty date parameter should return the current time in a JSON object with a unix key
+// Failed: An empty date parameter should return the current time in a JSON object with a utc key
+
 app.get('/api/:date?', (req, res) => {
   const queryParam = req.params;
 
